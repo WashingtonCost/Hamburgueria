774e6c995fdb9428e13fb63dff790c83cfcbe796
welingtonneto <84087866+welingtonneto@users.noreply.github.com>
2023-05-26 00:48:20 -0400

    atualização neto tudo

--- neto_formularios/login.js
+++ neto_formularios/login.js
@@ -3,3 +3,23 @@ function entradamenu (){
     
 } 
 
+$(document).ready(function() {
+    // Função para habilitar ou desabilitar o botão "Entrar"
+    function atualizarBotaoEntrar() {
+        var username = $("#username").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        if (username !== "" && email !== "" && password !== "") {
+            $("#entrar-btn").prop("disabled", false);
+        } else {
+            $("#entrar-btn").prop("disabled", true);
+        }
+    }
+
+    // Atualiza o estado do botão ao digitar nos campos
+    $("#username, #email, #password").on("input", function() {
+        atualizarBotaoEntrar();
+    });
+});
+
---
+++ neto_formularios/perfil.js
@@ -0,0 +1,27 @@
+
+
+  function previewImage(event) {
+    const input = event.target;
+    const preview = document.getElementById("imagePreview");
+
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const image = document.createElement("img");
+        image.src = e.target.result;
+        image.alt = "Preview da Imagem";
+        image.classList.add("thumbnail");
+
+        preview.innerHTML = "";
+        preview.appendChild(image);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    } else {
+      preview.innerHTML = "";
+    }
+  }
+
+ 
+
